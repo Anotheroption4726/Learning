93fe7de2da1c8c5b018c767f815048409d41b436
Mouth <napalm_ground@hotmail.com>
2019-12-05 19:06:01 +0100

    youtube part 2

---
+++ c_tutorial_youtube/part_02/io_demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int entier = 4;
+	printf("%d", entier);
+
+    char caractere = 'A';
+    printf("%c\n", caractere);
+
+    int entier_1 = 10;
+    printf("%d\n", entier_1);
+
+    int entier_2 = 12;
+    printf("%i\n", entier_2);
+    
+    return(0);
+}
